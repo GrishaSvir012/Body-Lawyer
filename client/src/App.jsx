5e2be43a9444eb40253c320fb958bb4d32c848ea
Elizaveta <veta.gubina@yandex.ru>
2022-08-19 19:04:25 +0300

    components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Container } from 'reactstrap';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+
 import HomePage from './components/HomePage/HomePage';
 import NavBar from './components/NavBar/NavBar';
+import './App.css';
+import SignUpLiza from './components/RegistrLiza/SignUpLiza';
+import LogInLiza from './components/RegistrLiza/LogInLiza';
 
 function App() {
   return (
-    <Container>
+    <Grid classes={{ root: { width: '100%' } }}>
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpLiza />} />
+        <Route path="/login" element={<LogInLiza />} />
       </Routes>
-    </Container>
+    </Grid>
   );
 }
 
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,47 +1,38 @@
 import React, { useState } from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  NavbarText,
-} from 'reactstrap';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
   return (
-    <div>
-      <Navbar>
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
-      </Navbar>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <RestaurantIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            <Button sx={{ color: '#fff' }}><Link to="/">адвокат тела</Link></Button>
+            <Box className="navbarButton">
+              <Button sx={{ color: '#fff' }}><Link to="/signup">зарегистрироваться</Link></Button>
+              <Button sx={{ color: '#fff' }}><Link to="/login">войти</Link></Button>
+            </Box>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
