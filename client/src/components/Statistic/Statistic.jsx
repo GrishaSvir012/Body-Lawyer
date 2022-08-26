21f86b857f38f975248478ebfc98273f11fde7e4
Merpert <merpertstepan@gmail.com>
2022-08-26 13:06:32 +0300

    Statistic done

--- client/src/components/Statistic/Statistic.jsx
+++ client/src/components/Statistic/Statistic.jsx
@@ -17,7 +17,6 @@ import { getUserBodyGet } from '../../Redux/actions/getBodyAction';
 ChartJS.register(Title, BarElement, LinearScale, CategoryScale, ArcElement, Tooltip, Legend);
 
 export default function Statistic({ stat }) {
-  console.log(stat, '{{{{EQQE{EQQEJJKEHJIHQEJ');
   const personInfo = useSelector((state) => state.getBodyInfo);
   const dateArr = stat.map((el) => el?.date);
   const dateArrReg = dateArr.map((el) => el.replace(/T21:00:00.000Z/gm, ''));
@@ -27,12 +26,58 @@ export default function Statistic({ stat }) {
   const newArrFats = stat.map((el) => el?.total_fats);
   const newArrCarb = stat.map((el) => el?.total_carbohidrates);
   const labels = dateArrReg.map((el) => el.replace(/\d*$/, (s) => (`${+s + 1}`)));
-  console.log(labels);
+
+  // console.log(labels);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getUserBodyGet());
   }, []);
-  console.log(personInfo, 'personInfo in stat');
+
+  const newArrKkalNeed = personInfo.map((el) => el?.calories_needed.toString());
+  const newArrProtNeed = personInfo.map((el) => el?.protein_needed.toString());
+  const newArrFatsNeed = personInfo.map((el) => el?.fats_needed.toString());
+  const newArrCarbNeed = personInfo.map((el) => el?.carbohidrates_needed.toString());
+  const labels2 = [`Калории: ${newArrKkalNeed} Белки: ${newArrProtNeed} Жиры: ${newArrFatsNeed} Углеводы: ${newArrCarbNeed}`];
+  const dataNeed = {
+
+    labels: labels2,
+    datasets: [
+      {
+        label: 'Калории',
+        data: newArrKkalNeed,
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Белки',
+        data: newArrProtNeed,
+        backgroundColor: 'rgba(53, 144, 111, 0.5)',
+      },
+      {
+        label: 'Жиры',
+        data: newArrFatsNeed,
+        backgroundColor: 'rgba(53, 170, 190, 0.5)',
+      },
+      {
+        label: 'Углеводы',
+        data: newArrCarbNeed,
+        backgroundColor: 'rgba(53, 100, 200, 0.5)',
+      },
+
+    ],
+  };
+  const optionsNeed = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Дневная норма питания',
+      },
+    },
+  };
+
   const data = {
 
     labels,
@@ -76,13 +121,13 @@ export default function Statistic({ stat }) {
   return (
     <Row className="twoStat">
       <Col>
-        <div style={{ width: '700px' }} className="imgStat">
+        <div style={{ width: '100%' }} className="imgStat">
           {/* <Doughnut data={data} /> */}
-          <Bar options={options} data={data} />
+          <Bar options={optionsNeed} data={dataNeed} />
         </div>
       </Col>
       <Col>
-        <div style={{ width: '700px' }}>
+        <div style={{ width: '100%' }}>
           {/* <Doughnut data={data} /> */}
           <Bar options={options} data={data} />
         </div>
@@ -90,30 +135,3 @@ export default function Statistic({ stat }) {
     </Row>
   );
 }
-
-// const data = {
-//   labels: data2,
-//   datasets: [
-//     {
-//       label: '# of Votes',
-//       data: kkal,
-//       backgroundColor: [
-//         'rgba(255, 99, 132, 0.2)',
-//         'rgba(54, 162, 235, 0.2)',
-//         'rgba(255, 206, 86, 0.2)',
-//         'rgba(75, 192, 192, 0.2)',
-//         'rgba(153, 102, 255, 0.2)',
-//         'rgba(255, 159, 64, 0.2)',
-//       ],
-//       borderColor: [
-//         'rgba(255, 99, 132, 1)',
-//         'rgba(54, 162, 235, 1)',
-//         'rgba(255, 206, 86, 1)',
-//         'rgba(75, 192, 192, 1)',
-//         'rgba(153, 102, 255, 1)',
-//         'rgba(255, 159, 64, 1)',
-//       ],
-//       borderWidth: 1,
-//     },
-//   ],
-// };
